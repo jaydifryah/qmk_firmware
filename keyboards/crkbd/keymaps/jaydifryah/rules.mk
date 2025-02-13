e3e83eb96545d5ada02f833cbd475be601840a23
jaydifryah <47575592+jaydifryah@users.noreply.github.com>
2025-02-13 14:30:10 -0800

    Adding previous community keymaps
    Community / personal keymaps were removed from the latest sync of QMK, re-added

---
+++ keyboards/crkbd/keymaps/jaydifryah/rules.mk
@@ -0,0 +1,9 @@
+# Bootloader selection
+#BOOTLOADER = qmk-dfu
+
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+NKRO_ENABLE     = yes
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
